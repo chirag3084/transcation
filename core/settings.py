76b65a2d5e701dde09b24197112ca7f1431fb117
cody <110027137+chirag3084@users.noreply.github.com>
2025-08-06 23:45:48 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -96,6 +96,22 @@ WSGI_APPLICATION = "core.wsgi.application"
 # }
 
 
+import psycopg2
+
+# Load the environment variable
+database_url = os.getenv('DATABASE_URL')
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(database_url)
+
+with conn.cursor() as cur:
+    cur.execute("SELECT version()")
+    print(cur.fetchone())
+
+# Close the connection
+conn.close()
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
