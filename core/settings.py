d114f1b932043b7443591c6c39c028c912190f71
chirag3084 <surtivatsal23@gmail.com>
2026-06-18 09:54:30 +0530

    allowed host and debug config

--- core/settings.py
+++ core/settings.py
@@ -36,9 +36,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 LOGIN_URL = "login"
 
 
